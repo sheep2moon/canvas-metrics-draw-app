59cb07de99059061ab9a412ec993a7d0438f8f7b
Konrad Iwan <kon.iwan17@gmail.com>
2023-02-15 19:59:05 +0100

    changing grid snapping to be more precise

--- src/helpers/snapToGrid.ts
+++ src/helpers/snapToGrid.ts
@@ -1,6 +1,6 @@
 export const toSnappedCoords = (x: number, y: number, grid_size: number): Position => {
-    const h_count = Math.ceil(x / grid_size);
-    const v_count = Math.ceil(y / grid_size);
+    const h_count = Math.round(x / grid_size);
+    const v_count = Math.round(y / grid_size);
 
     return { x: h_count * grid_size, y: v_count * grid_size };
 };
